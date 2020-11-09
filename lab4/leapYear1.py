e0f75063414dcd72c9333f18300b82cf8f613cc5
p61061162 <morpheuskafasiguzel@gmail.com>
2020-11-09 09:11:28 +0000

    sumlist

---
+++ lab4/leapYear1.py
@@ -0,0 +1,5 @@
+year = int(input("please enter a year to calculate leap year: "))
+
+if (year % 4) == 0:
+  if (year % 100) == 0:
+
---
+++ lab4/sumList1.py
@@ -0,0 +1,4 @@
+nums = [8,60,43,55,25,134,1]
+
+Sum = sum (nums)
+print (Sum)
